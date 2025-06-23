75f50103eb10f0177d23a93f336ac8515255e0ee
Francisco Leite <franciscoleite.dev@protonmail.com>
2025-06-23 23:39:22 +0000

    Add c ring_buffer

--- c/ring_buffer.c
+++ c/ring_buffer.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 
 int main(void) {
-  int *ring_buffer[7];
+  int size = 7;
+  int *ring_buffer[size];
 
-  int value = 20;
-  int read_index = -1;
-  int write_index = -1;
+  int read_index = 0;
+  int write_index = 0;
   while (1) {
-    int key[1];
-    printf("Enter the key");
-    while ((key = getchar()) != EOF) {
-      putchar(key);
+    int key;
+    printf("Enter the key\n");
+    scanf("%d", &key);
+
+    if (write_index == size) {
+      write_index = 0;
     }
 
-    if (write_index == -1) {
-      ring_buffer[0] = &key;
+    if (write_index == 0) {
+      ring_buffer[write_index] = &key;
+      write_index++;
+      printf("Current write index 0\n");
+      continue;
     }
     else {
+      write_index++;
       ring_buffer[write_index] = &key;
     }
+
+    if (key % 2 == 0) {
+      read_index++;
+    }
+    printf("Current write index %d\n", write_index);
+    printf("Current read index %d\n", read_index);
   }
+
+  return 0;
 }
