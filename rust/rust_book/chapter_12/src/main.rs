f06def104378427627907d4dd2cb12bb8b10c67b
Francisco Leite <franciscoleite.dev@protonmail.com>
2025-07-21 22:59:25 +0000

    Refactoring rust book chapter_12

--- rust/rust_book/chapter_12/src/main.rs
+++ rust/rust_book/chapter_12/src/main.rs
@@ -5,9 +5,8 @@ use chapter_12::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = chapter_12::Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
-        panic!("Problem parsing arguments: {}", err);
     });
 
     if let Err(e) = chapter_12::run(config) {
